f0d475917d1980f97a54f4f72dd03b8be8c7120b
_shark <stanislav.kozorez@gmail.com>
2016-06-02 15:20:42 +0300

    add model validation

--- BlogHost/Models/LoginViewModel.cs
+++ BlogHost/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,11 @@ namespace BlogHost.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "Password must contain from 3 to 50 characters")]
         public string Password { get; set; }
     }
 }
--- BlogHost/Models/RegistrationViewModel.cs
+++ BlogHost/Models/RegistrationViewModel.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace BlogHost.Models
 {
-    public class RegistrationViewModel
+    public class RegistrationViewModel: IValidatableObject
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "Password must contain from 3 to 50 characters")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "Password must contain from 3 to 50 characters")]
         public string RepeatPassword { get; set; }
 
-        //public string Capcha { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (Password != RepeatPassword)
+                errors.Add(new ValidationResult("Passwords do not match", new[] { "Password", "RepeatPassword" }));
+            return errors;
+        }
     }
 }
