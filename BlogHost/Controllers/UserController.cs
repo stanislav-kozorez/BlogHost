b1ad515116515959aca292a0292916b7ac526d49
_shark <stanislav.kozorez@gmail.com>
2016-06-03 17:16:10 +0300

    add user management page for admin

---
+++ BlogHost/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ORM.Entity;
+using ORM;
+using System.Collections.Generic;
+using BlogHost.Models;
+using System.Web.Helpers;
+
+namespace BlogHost.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            using (var context = new BlogHostDbContext())
+            {
+                List<User> users = context.Users.Include("Role").ToList();
+                return View(users);
+            }
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index");
+            using (var context = new BlogHostDbContext())
+            {
+                var ormUser = context.Users.Find(id);
+                if(ormUser != null)
+                {
+                    var editUser = new UserEditViewModel();
+                    editUser.UserId = ormUser.UserId;
+                    editUser.Name = ormUser.Name;
+                    editUser.Email = ormUser.Email;
+                    editUser.RoleId = ormUser.Role.RoleId;
+                    editUser.Roles = new SelectList(context.Roles.ToList(), "RoleId", "Name");
+                   
+                    //ViewBag.Roles = new SelectList(context.Roles.ToList(), "RoleId", "Name", ormUser.Role.RoleId);
+
+                    //ViewBag.Roles = context.Roles.ToList().Select(x => new SelectListItem() {Text = x.Name, Value = x.RoleId.ToString(), Selected = x.RoleId == ormUser.Role.RoleId });
+                    return View(editUser);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserEditViewModel editUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new BlogHostDbContext())
+                {
+                    var ormUser = context.Users.Find(editUser.UserId);
+                    if (ormUser != null)
+                    {
+                        ormUser.Name = editUser.Name;
+                        if (editUser.Password != null && !Crypto.VerifyHashedPassword(ormUser.Password, editUser.Password))
+                            ormUser.Password = Crypto.HashPassword(editUser.Password);
+                        ormUser.Role = context.Roles.Find(editUser.RoleId);
+                        context.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(editUser);
+        }
+    }
+}
---
+++ BlogHost/Models/UserEditViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogHost.Models
+{
+    public class UserEditViewModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; } 
+        public SelectList Roles { get; set; }
+    }
+}
